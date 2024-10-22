8908fcebf04b2f6b30d20a5f26648533271aecfe
AaNn05 <117485357+AaNn05@users.noreply.github.com>
2024-10-22 16:43:51 +0400

    Add files via upload

---
+++ Palindrome.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class Palindrome
+    {
+        public static bool checker(string word, int n, bool result)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == word[n - 1] && n >= 0)
+                {
+                    n = n - 1;
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Algorithms;
+
+Console.Write("Input a word: ");
+string word = Console.ReadLine().ToLower();
+
+int n = word.Length;
+bool result = true;
+
+Console.WriteLine(Palindrome.checker(word, n, result));
